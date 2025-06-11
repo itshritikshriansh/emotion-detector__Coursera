4fb76d8ec0efa1ffa316acda159fefcf7d6f68e3
Hritik Shriansh <hritikshriansh@gmail.com>
2025-06-11 07:19:19 -0400

    Incorporated error handling (Task 7)

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -4,11 +4,15 @@ def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     object =  { "raw_document": { "text": str(text_to_analyze) } }
-
+    final_output = {'anger': None, 'disgust': None, 'fear': None, 'joy': None, 'sadness': None, 'dominant_emotion': None}
+    
     response = requests.post(url, json = object, headers = header)
+
+    if response.status_code == 400:
+        return final_output
     
     formatted_response = response.json()
-    # print(formatted_response)
+    print(formatted_response)
     anger = formatted_response['emotionPredictions'][0]['emotion']['anger']
     disgust = formatted_response['emotionPredictions'][0]['emotion']['disgust']
     fear = formatted_response['emotionPredictions'][0]['emotion']['fear']
