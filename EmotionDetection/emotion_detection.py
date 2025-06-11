4b9398218179dad049d2c50869104430abdd4c05
Hritik Shriansh <hritikshriansh@gmail.com>
2025-06-11 06:54:05 -0400

    Bug fix

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -3,11 +3,12 @@ import requests, json
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    object =  { "raw_document": { "text": text_to_analyze } }
+    object =  { "raw_document": { "text": str(text_to_analyze) } }
 
     response = requests.post(url, json = object, headers = header)
     
     formatted_response = response.json()
+    # print(formatted_response)
     anger = formatted_response['emotionPredictions'][0]['emotion']['anger']
     disgust = formatted_response['emotionPredictions'][0]['emotion']['disgust']
     fear = formatted_response['emotionPredictions'][0]['emotion']['fear']
@@ -29,5 +30,7 @@ def emotion_detector(text_to_analyze):
     
     else:
         dominant_emotion = 'sadness'
+
+    final_output = {'anger': anger, 'disgust': disgust, 'fear': fear, 'joy': joy, 'sadness': sadness, 'dominant_emotion': dominant_emotion}
     
-    return {'anger': anger, 'disgust': disgust, 'fear': fear, 'joy': joy, 'sadness': sadness, 'dominant_emotion': dominant_emotion}
+    return final_output
--- server.py
+++ server.py
@@ -1,16 +1,22 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from EmotionDetection import emotion_detector
 
 app = Flask('Emotion Detection using Watson NLP library')
 
-@app.route("/")
+@app.route('/')
+def show_index():
+    return render_template('index.html')
+
+
+@app.route("/emotionDetector")
 def emotionDetector():
-    text = request.args.get('textToAnalyze')
+    text = str(request.args.get('textToAnalyze'))
     emotion_val = emotion_detector(text)
 
     final_output = f"For the given statement, the system response is 'anger': {emotion_val['anger']}, 'disgust': {emotion_val['disgust']}, 'fear': {emotion_val['fear']}, 'joy': {emotion_val['joy']}, 'sadness': {emotion_val['sadness']}. The dominant emotion is: {emotion_val['dominant_emotion']}"
 
     return final_output
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
